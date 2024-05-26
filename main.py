80dfbcf5c815f4ef6dc37870d2a691ba24f3bbba
Yoannoza <yoannoza25@gmail.com>
2024-05-26 19:11:50 +0100

    Update to just image detection

--- main.py
+++ main.py
@@ -1,77 +1,35 @@
-"""Object detection demo with MobileNet SSD.
-This model and code are based on
-https://github.com/robmarkcole/object-detection-app
-"""
-
-import av
-import cv2
-import numpy as np
-import supervision as sv
-from ultralytics import YOLO
 import streamlit as st
-from streamlit_webrtc import WebRtcMode, webrtc_streamer
+from ultralytics import YOLO
+from PIL import Image
+import requests
+from io import BytesIO
 
+# Charger le modèle YOLO
+model = YOLO("yolov8n.pt")  # charger un modèle officiel
 
-ZONE_POLYGON = np.array([
-    [0, 0],
-    [0.5, 0],
-    [0.5, 1],
-    [0, 1]
-])
+st.title("Détection d/'objets avec YOLOv8")
 
-model = YOLO("yolov8l.pt")
+# Uploader une image
+uploaded_file = st.file_uploader("Choisissez une image...", type=["jpg", "jpeg", "png"])
 
-box_annotator = sv.BoxAnnotator(
-    thickness=2,
-    text_thickness=2,
-    text_scale=1
-)
-
-zone_polygon = (ZONE_POLYGON).astype(int)
-zone = sv.PolygonZone(polygon=zone_polygon, )
-zone_annotator = sv.PolygonZoneAnnotator(
-    zone=zone, 
-    color=sv.Color.BLACK, 
-    thickness=2,
-    text_thickness=4,
-    text_scale=2
-)
-
-
-def video_frame_callback(frame: av.VideoFrame) -> av.VideoFrame:
-
-    result = model(frame, agnostic_nms=True)[0]
-    detections = sv.Detections.from_yolov8(result)
-    labels = [
-        f"{model.model.names[class_id]} {confidence:0.2f}"
-        for _, confidence, class_id, _
-        in detections
-    ]
-    frame = box_annotator.annotate(
-        scene=frame, 
-        detections=detections, 
-        labels=labels
-    )
-
-    zone.trigger(detections=detections)
-    frame = zone_annotator.annotate(scene=frame)    
-
-    return frame
-
-
-webrtc_ctx = webrtc_streamer(
-    key="object-detection",
-    mode=WebRtcMode.SENDRECV,
-    rtc_configuration={
-        "iceTransportPolicy": "relay",
-    },
-    video_frame_callback=video_frame_callback,
-    media_stream_constraints={"video": True, "audio": False},
-    async_processing=True,
-)
-
-st.markdown(
-    "This demo uses a model and code from "
-    "https://github.com/robmarkcole/object-detection-app. "
-    "Many thanks to the project."
-)
+if uploaded_file is not None:
+    # Lire l'image
+    image = Image.open(uploaded_file)
+    
+    # Afficher l'image téléchargée
+    st.image(image, caption="Image téléchargée", use_column_width=True)
+    st.write("")
+    st.write("Détection en cours...")
+    
+    # Sauvegarder l'image téléchargée pour que YOLO puisse la lire
+    image.save("uploaded_image.jpg")
+    
+    # Prédiction avec le modèle
+    results = model("uploaded_image.jpg")
+    
+    # Sauvegarder le résultat
+    results.save("result.jpg")
+    
+    # Afficher le résultat
+    result_image = Image.open("result.jpg")
+    st.image(result_image, caption='Résultat de la détection', use_column_width=True)
